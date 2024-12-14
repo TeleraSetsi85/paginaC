2ef42f185f64f10eafa0ebb5a0f9e987e535bdc2
Diego Alfonso <darellano1@ucol.mx>
2024-12-14 09:10:59 -0600

    Una pequeña modificacion, solo adelante poco el srcipt

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+//Nav bar functions
+document.getElementById('Acerca-de').addEventListener('click', ()=>{
+    //Que scrolie hasta donde dice quien es
+});
+
+document.getElementById('Redes-sociales').addEventListener('click', ()=>{
+    //Que scrolie hasta el footer
+});
+
+document.getElementById('contacto').addEventListener('click', ()=>{
+    //Ventana emergente un pop up
+})
