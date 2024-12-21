a37d9e0aa0cc46f3821e923d92d3479636068dd9
Diego Alfonso <darellano1@ucol.mx>
2024-12-21 15:24:52 -0600

    Se agrego funciones basicas

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,34 @@
 //Nav bar functions
 document.getElementById('Acerca-de').addEventListener('click', ()=>{
-    //Que scrolie hasta donde dice quien es
+    const section = document.getElementById('who-section'); //Read the section
+    section.scrollIntoView({behavior: 'smooth'}); //scroll to the section
 });
 
 document.getElementById('Redes-sociales').addEventListener('click', ()=>{
-    //Que scrolie hasta el footer
+    const sectionFooter = document.getElementById('section-footer');
+    sectionFooter.scrollIntoView({behavior: 'smooth'});
 });
 
-document.getElementById('contacto').addEventListener('click', ()=>{
-    //Ventana emergente un pop up
-});
+// Referencias a los elementos
+const contacto = document.getElementById('contacto');
+const popUp = document.getElementById('contacto-pop');
+const closeModal = document.getElementById('close-modal');
+
+// Función para abrir el modal
+const abrirModal = () => {
+    popUp.showModal();
+};
+
+// Función para cerrar el modal
+const cerrarModal = () => {
+    popUp.close();
+};
+
+// Se asigna a los eventos
+contacto.addEventListener('click', abrirModal);
+closeModal.addEventListener('click', cerrarModal);
+
+
 
 document.getElementById('register').addEventListener('click', ()=>{
     window.location.href = 'register.html';
