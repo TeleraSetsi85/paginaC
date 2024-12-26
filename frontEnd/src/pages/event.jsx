9f1ebd0f97a19539cd236fd933d0b11c3af854e2
Diego Alfonso <darellano1@ucol.mx>
2024-12-26 14:10:05 -0600

    Estructura principal de los eventos

--- frontEnd/src/pages/event.jsx
+++ frontEnd/src/pages/event.jsx
@@ -12,12 +12,27 @@ function events(){
                 {/*Agregar un timer del evento mas cercano*/}
             </nav>
             <main>
-                /*primer evento */
+                {/*Los eventos apareceran segun los coloque el autor*/}
+                {/*primer evento */}
                 <section>
                     <div>
                         <h3>Nombre del evento</h3>
+                        <img src="" alt="photo-event" />
+                        <p>00/00/00</p>{/*Fecha de cuando es*/}
+                        <p>00:00:00</p> {/*Si se puede poner un timer de cuanto falta*/}
+                        <p>{/*Una descripcion del evento*/}</p>
                     </div>
+                    <div>
+                        <label htmlFor="">Nombre Completo</label>
+                        <input type="text"  placeholder="Ingresa nombre completo"/>
+                        <button>Comprar ahora</button>
+                    </div>
+
                 </section>
+                {/*Metodo de pago*/}
+                <dialog>
+
+                </dialog>
             </main>
         </>
     
